fdf2086fb807b6fbe6de9f09ff3e1d27e2aed57e
Mohmmad Sarim <mohdsarim8@gmail.com>
2025-06-24 10:13:42 +0530

    mlflow

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,9 +1,6 @@
 import os
 import sys
 from dataclasses import dataclass
-from urllib.parse import urlparse
-import mlflow
-import mlflow.sklearn
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from catboost import CatBoostRegressor
@@ -13,9 +10,12 @@ from sklearn.ensemble import (
     RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+
+import mlflow
+import mlflow.sklearn
+import dagshub
 
 from src.exception import CustomException
 from src.logger import logging
@@ -30,10 +30,12 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
-        mlflow.set_tracking_uri("https://dagshub.com/mohdsarim8/mlproject.mlflow")
-        mlflow.set_registry_uri("https://dagshub.com/mohdsarim8/mlproject.mlflow")
 
-    def eval_metrics(self, actual, pred):
+        # Initialize Dagshub integration for MLflow
+        dagshub.init(repo_owner='mohdsarim8', repo_name='mlproject', mlflow=True)
+
+    @staticmethod
+    def eval_metrics(actual, pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         r2 = r2_score(actual, pred)
@@ -60,36 +62,20 @@ class ModelTrainer:
             }
 
             params = {
-                "Decision Tree": {
-                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                },
-                "Random Forest": {
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                },
+                "Decision Tree": {'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']},
+                "Random Forest": {'n_estimators': [8, 16, 32, 64, 128, 256]},
                 "Gradient Boosting": {
                     'learning_rate': [0.1, 0.01, 0.05, 0.001],
                     'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
                 "Linear Regression": {},
-                "XGBRegressor": {
-                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                },
-                "CatBoosting Regressor": {
-                    'depth': [6, 8, 10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "AdaBoost Regressor": {
-                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                }
+                "XGBRegressor": {'learning_rate': [0.1, 0.01, 0.05, 0.001], 'n_estimators': [8, 16, 32, 64, 128, 256]},
+                "CatBoosting Regressor": {'depth': [6, 8, 10], 'learning_rate': [0.01, 0.05, 0.1], 'iterations': [30, 50, 100]},
+                "AdaBoost Regressor": {'learning_rate': [0.1, 0.01, 0.5, 0.001], 'n_estimators': [8, 16, 32, 64, 128, 256]}
             }
 
-            model_report, trained_models = evaluate_models(
-                X_train, y_train, X_test, y_test, models, params
-            )
+            model_report, trained_models = evaluate_models(X_train, y_train, X_test, y_test, models, params)
 
             best_model_name = max(model_report, key=lambda x: model_report[x]["test_score"])
             best_model_score = model_report[best_model_name]["test_score"]
@@ -101,7 +87,10 @@ class ModelTrainer:
 
             logging.info(f"Best Model: {best_model_name} with R2 score: {best_model_score}")
 
+            ### MLflow Tracking ###
             with mlflow.start_run(run_name=best_model_name):
+                
+                mlflow.log_param("model_name", best_model_name)
                 mlflow.log_params(best_params)
 
                 predicted = best_model.predict(X_test)
@@ -113,6 +102,7 @@ class ModelTrainer:
 
                 mlflow.sklearn.log_model(best_model, "model")
 
+            ### Save model locally ###
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
